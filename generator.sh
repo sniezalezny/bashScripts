780222a385b5bcb6c4a79723ee1bc45ce467bd4e
sniezalezny <szymon.slowicki@gmail.com>
2021-04-12 23:32:18 +0200

    Generator $RANDOM

--- generator.sh
+++ generator.sh
@@ -4,7 +4,7 @@ ilosc_linii=2000
 
 for (( i=1; $i <= $ilosc_linii; i++ ))
 do
-        ilosc_liczb=$(($RANDOM % 7))
+        ilosc_liczb=$(($RANDOM % 9))
         #echo $ilosc_liczb
         j=1
         while [ $j -le $ilosc_liczb  ]
